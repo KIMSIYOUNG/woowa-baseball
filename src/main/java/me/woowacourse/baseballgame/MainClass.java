1360a2a5fab44beea2192adab178b6d534c08c81
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-25 17:45:44 +0900

    test: 1-9사이의 랜덤값이 정상적으로 나오는지 테스트

--- src/main/java/me/woowacourse/baseballgame/MainClass.java
+++ src/main/java/me/woowacourse/baseballgame/MainClass.java
@@ -5,40 +5,40 @@ import java.util.*;
 public class MainClass {
     private static final Scanner sc = new Scanner(System.in);
     private static String userInput;
-    private static int strike = 0;
-    private static int ball = 0;
-    private static int nothing = 0;
-
+    private static final int END_GAME = 2;
 
     public static void main(String[] args) {
         while (true) {
             System.out.println("게임을 새로 시작하려면 1번, 종료하려면 2번을 입력해주세요.");
-            int gameStart = sc.nextInt();
-            if (gameStart == 2)
+            int game = sc.nextInt();
+            if (game == END_GAME)
                 break;
             String randomNumber = makeRandomNumber();
-            System.out.println("숫자를 입력해주세요  ");
+            System.out.println("세자리 수를 입력해주세요  ");
             do {
                 System.out.print("숫자를 입력해주세요 --> 결과 :  ");
                 userInput = String.valueOf(sc.nextInt());
                 validateMethod(userInput, randomNumber);
-                strike = 0;
-                ball = 0;
-                nothing = 0;
+
             } while (!(userInput.equals(randomNumber)));
             System.out.println("정답을 맞히셨습니다.");
-
         }
     }
 
     private static void validateMethod(String userInput, String randomNumber) {
+        int strike = 0;
+        int ball = 0;
+        int nothing = 0;
         String[] userInputArray = userInput.split("");
         String[] randomNumberArray = randomNumber.split("");
         List<String> randomNumberList = Arrays.asList(randomNumberArray);
+
         for (int i = 0; i < userInputArray.length; i++) {
-            if (randomNumberList.contains(userInputArray[i]) && randomNumberList.indexOf(userInputArray[i]) == i) {
+            if (randomNumberList.contains(userInputArray[i])
+                    && randomNumberList.indexOf(userInputArray[i]) == i) {
                 strike++;
-            } else if (randomNumberList.contains(userInputArray[i]) && randomNumberList.indexOf(userInputArray[i]) != i) {
+            } else if (randomNumberList.contains(userInputArray[i])
+                    && randomNumberList.indexOf(userInputArray[i]) != i) {
                 ball++;
             } else if (!randomNumberList.contains(userInputArray[i]))
                 nothing++;
--- src/test/java/me/woowacourse/baseballgame/BaseballgameApplicationTests.java
+++ src/test/java/me/woowacourse/baseballgame/BaseballgameApplicationTests.java
@@ -5,16 +5,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BaseballgameApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-    @Test
     public void helloWorld(){
         String helloWorld = "Hello world";
         assertEquals(helloWorld,"Hello world");
+    }
+    @Test
+    public void ValidateRandomNumber(){
+        int randomNumber = (int) (Math.random() * 9) + 1;
+        for(int i = 0; i < 100000; i++){
+            assertTrue(randomNumber>=1);
+            assertTrue(randomNumber<=9);
+        }
     }
 }
